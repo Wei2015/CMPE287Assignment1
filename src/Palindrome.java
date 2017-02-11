9311a258afe20620afd695cce560bb36dcaccb40
Wei Yao <wei.yao@sjsu.edu>
2017-02-10 16:34:32 -0800

    add punctuation mark testing method.

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,24 +1,37 @@
 /**
- * Created by weiyao on 2/4/17.
+ * This class contains a static tool to check if a string is palindrome or not.
  */
 public class Palindrome {
 
+    /**
+     * This method will ignore input with white spaces. It will only recognize numbers and letters in case-insensitive manner.
+     * @param input a string
+     * @return true if input is palindrome
+     */
     public static boolean isPalindrome(String input) {
-            if (input == null || input.isEmpty()) {
+        //input is null, throw exception
+        if (input == null)  {
+                throw new IllegalArgumentException();
+        }
+
+        //remove all white space
+        String temp = input.replaceAll(" ", "").trim().toLowerCase();
+
+        //check for punctuation marks
+        if (temp.matches(".*[^a-zA-Z0-9].*")) {
+            return false;
+        }
+
+        //check for palindrome
+        int tempLength = temp.length();
+
+        for (int i = 0; i < tempLength/2; i++ ) {
+            if(temp.charAt(i) != temp.charAt(tempLength-1-i)) {
                 return false;
             }
+        }
 
-            String temp = input.replaceAll("\\W", "").trim().toLowerCase();
-
-            int tempLength = temp.length();
-
-            for (int i = 0; i < tempLength/2; i++ ) {
-                if(temp.charAt(i) != temp.charAt(tempLength-1-i)) {
-                    return false;
-                }
-            }
-
-            return true;
+        return true;
 
     }
 
--- tests/PalindromeTest.java
+++ tests/PalindromeTest.java
@@ -1,44 +1,60 @@
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by weiyao on 2/4/17.
+ * This test program contain five test cases.
  */
 public class PalindromeTest {
+
     @Test
-    public void whiteSpace()  {
+    public void punctuationMark() throws Exception {
 
-        String str = "   Hello ? =!  -olleh";
+        for (int i = 33; i < 127; i++) {
+            String str = Character.toString((char)i);
+            if (!str.matches("[a-zA-Z0-9]")) {
+                str = "hello"  + Character.toString((char)i) + "olleh";
+                assertFalse(Palindrome.isPalindrome(str));
+            }
+        }
+    }
 
+    @Test
+    public void whiteSpace() throws Exception {
+        String str = "   hello   olleh   ";
         assertTrue(Palindrome.isPalindrome(str));
     }
 
     @Test
     public void caseSensitive() {
-
         String str = "aBcCBA";
-
         assertTrue(Palindrome.isPalindrome(str));
 
     }
 
     @Test
-    public void numberTest() {
+    public void numberTest() throws Exception {
         String str = "123832";
-
         assertFalse(Palindrome.isPalindrome(str));
+
+        str = "12321";
+        assertTrue(Palindrome.isPalindrome(str));
 
     }
 
-
     @Test
-    public void emptyString() {
+    public void emptyString() throws Exception {
         String str = "";
+        assertTrue(Palindrome.isPalindrome(str));
+    }
 
-        assertFalse(Palindrome.isPalindrome(str));
+    @Test (expected = IllegalArgumentException.class)
+    public void nullInput() throws Exception {
+        String str = null;
+        Palindrome.isPalindrome(str);
     }
 
 
